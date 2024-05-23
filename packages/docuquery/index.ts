6a4961463a463a776e0f24c2bc94415ab260f288
Gah Tang <gkt5015@gmail.com>
2024-05-23 16:25:34 -0400

    Allow sub and unsub of op

--- packages/docuquery/index.ts
+++ packages/docuquery/index.ts
@@ -1,5 +1,12 @@
 // Combines tanstack query with docucache to provide a fine-grained reactive query system
-import {QueryClient, type FetchQueryOptions, type InvalidateQueryFilters, type InvalidateOptions} from '@tanstack/query-core';
+import {
+  QueryClient,
+  type FetchQueryOptions,
+  type InvalidateQueryFilters,
+  type InvalidateOptions,
+  type QueryObserverResult,
+  QueryObserver,
+} from '@tanstack/query-core';
 import {DocuStore, type DocucacheInitOptions } from '@docucache/docucache';
 
 const docOpKeySymbol = Symbol.for('__DocOpKey');
@@ -17,6 +24,7 @@ type DocuOperationConfig<T extends any[], R> = {
 
 type OpFn<T extends any[], R> = ((...args: T) => Promise<R> | R) & {
   [docOpKeySymbol]?: ReadonlyArray<unknown> | ((...args: T) => ReadonlyArray<unknown>);
+  subscribe: (cb: (result :QueryObserverResult) => void) => () => void;
 };
 
 class DocuQueryClient {
@@ -50,32 +58,51 @@ class DocuQueryClient {
   }
 
   operation<T extends any[], R>(config: DocuOperationConfig<T, R>) {
+    let unsubscribe: () => void;
+    let callback: ((result: QueryObserverResult) => void) | null;
+    let observer: QueryObserver<R, Error, unknown, unknown, readonly unknown[]>;
     const op: OpFn<T, R> = (...args: T) => {
       let operationKey = config.operationKey;
       if(typeof config.operationKey === 'function') {
         operationKey = config.operationKey(...args);
       }
       operationKey = ['op', ...operationKey as ReadonlyArray<unknown>]
-      return this.queryClient.fetchQuery<R>({
-        // meta: {},
-        queryKey: operationKey,
-        queryFn: async () => {
-          try {
-            const result = await config.operationFn(...args);
-            if(config?.invalidate) {
-              const filters = 'queryKey' in config.invalidate ? config.invalidate : (config.invalidate as any).filters;
-              const options = 'queryKey' in config.invalidate ? {} : (config.invalidate as any).options;
-              await this.queryClient.invalidateQueries(filters, options)
-            }
-            return result;
-          } catch(err) {
-            // TODO: implement rollback? This might need to be in the queryClient subscription instead??
-            throw err;
+      const queryKey = operationKey;
+      const queryFn = async () => {
+        try {
+          const result = await config.operationFn(...args);
+          if(config?.invalidate) {
+            const filters = 'queryKey' in config.invalidate ? config.invalidate : (config.invalidate as any).filters;
+            const options = 'queryKey' in config.invalidate ? {} : (config.invalidate as any).options;
+            await this.queryClient.invalidateQueries(filters, options)
           }
-        },
-      });
+          return result;
+        } catch(err) {
+          // TODO: implement rollback? This might need to be in the queryClient subscription instead??
+          throw err;
+        }
+      };
+      if(!observer) {
+        observer = new QueryObserver(this.queryClient, {queryKey, queryFn});
+      }
+      observer.setOptions({queryKey, queryFn});
+      // Unsubscribe an reregister automatically
+      if(callback) {
+        unsubscribe?.();
+        unsubscribe = observer.subscribe(callback);
+      }
+      return this.queryClient.fetchQuery({queryKey, queryFn});
     };
     op[docOpKeySymbol] = config.operationKey;
+    op.subscribe = (cb) => {
+      callback = cb;
+      unsubscribe = observer.subscribe(cb);
+      // Return it like this because unsubscribe can change based on each call
+      return () => {
+        unsubscribe();
+        callback = null; // Clear this if manually unsubscribed
+      }
+    }
     return op;
   }
 
--- packages/docuquery/tests/main.test.ts
+++ packages/docuquery/tests/main.test.ts
@@ -1,58 +1,117 @@
-import {describe, it, mock} from 'bun:test';
+import {describe, expect, it, mock} from 'bun:test';
 import {sleep} from 'bun';
 import {createClient} from '../';
 
-describe('basic', () => {
-  it('works', async () => {
-    const petStore = [
+const tabStore = [
+  {
+    __typename: 'Tab',
+    id: '1',
+    name: 'Bill\'s Tab',
+    total: 400,
+    orders: [
       {
-        __typename: 'Pet',
+        __typename: 'Order',
         id: '1',
-        name: 'Biscuit',
-        breed: 'Cat',
+        total: 200,
+        items: [
+          {
+            __typename: 'Item',
+            id: '1',
+            name: 'Pizza',
+            price: 100,
+            quantity: 2
+          },
+        ]
       },
       {
-        __typename: 'Pet',
+        __typename: 'Order',
         id: '2',
-        name: 'Hamtaro',
-        breed: 'Hamster',
+        total: 200,
+        items: [
+          {
+            __typename: 'Item',
+            id: '2',
+            name: 'Burger',
+            price: 200,
+            quantity: 1
+          }
+        ]
       }
-    ];
-    
-    const client = createClient();
+    ]
+  },
+  {
+    __typename: 'Tab',
+    id: '2',
+    name: 'Jane\'s Tab',
+    total: 100,
+    orders: [
+      {
+        __typename: 'Order',
+        id: '3',
+        total: 100,
+        items: [
+          {
+            __typename: 'Item',
+            id: '3',
+            name: 'Fries',
+            price: 100,
+            quantity: 1
+          }
+        ]
+      }
+    ]
+  }
+];
 
-    const addPet = client.operation({
-      operationKey(pet: any) {
-        return ['addPet', pet.id];
-      },
-      async operationFn(pet: any) {
-        console.log('Adding pet');
-        const id = (petStore.length + 1).toString();
-        petStore.push({...pet, __typename: 'Pet', id});
-        // Sleep for 1 second to simulate a network request
-        await sleep(1000);
-        return null;
-      },
+const listTabsByIds = (filter?: string[]) => {
+  return tabStore.filter(tab => !filter || filter.includes(tab.id));
+}
+
+describe('operations', () => {
+  const client = createClient();
+  const listTabs = client.operation({
+    operationKey: (ids) => ['listTabs', ...ids || []],
+    async operationFn(ids?: string[]) {
+      await sleep(1000);
+      return listTabsByIds(ids);
+    },
+  });
+
+  let unsub: () => void;
+
+  it('will return basic query', async () => {
+    const tabs = await listTabs();
+    expect(tabs.length).toBe(tabStore.length);
+  });
+
+  it('will return filtered query', async () => {
+    const tabs = await listTabs(['2']);
+    expect(tabs.length).toBe(1);
+  });
+
+  it('can be subscribed to', async (done) => {
+    unsub = await listTabs.subscribe((result) => {
+      const data = result.data;
+      expect(data.length).toBe(1);
+      done();
     });
-    const listPets = client.operation({
-      operationKey: ['listPets'],
-      async operationFn() {
-        console.log('Listing pets');
-        await sleep(1000);
-        return petStore;
-      },
+  });
+
+  it('changing filter will automatically resub to same cb without needing to manually unsub', async (done) => {
+    unsub();
+    let cbCount = 0;
+    let expectedTabs = [['2'], ['1', '2']]; // First one should only be subscribed to tab 2, and then the second one should be subscribed to all tabs
+    const receivedTabs: any[] = [];
+    await listTabs(['2']); 
+    const callback = mock((result) => {
+      cbCount++;
+      receivedTabs.push(result.data.map(tab => tab.id));
+      if(cbCount === 2) {
+        expect(receivedTabs).toStrictEqual(expectedTabs);
+        done();
+      }
     });
-    const callback = mock((snapshot) => {
-      console.log('Query data updated');
-      console.log(snapshot);
-    });
-    // console.log(pets);
-    // This will be called when the listPets query is updated
-    client.subscribe(listPets, callback);
-    let pets = await listPets();
-    // console.log(pets);
-    await addPet({name: 'Fluffy', breed: 'Dog'});
-    pets = await listPets();
-    // callback should be called a few times
+    await listTabs.subscribe(callback);
+    await listTabs();
   });
 });
